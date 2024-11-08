5719ae393f2ad86f98d8f14717525aec1552cac5
vunv-15 <nguyenvu15309@gmail.com>
2024-11-08 23:07:30 +0700

    init project

--- commons/env.php
+++ commons/env.php
@@ -1,11 +1,12 @@
 <?php 
 
+// Đường dẫn vào Admin
 define('BASE_URL'       , 'http://localhost/mvc-oop-basic/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'db');  // Tên database
+define('DB_NAME'    , 'du_an_1');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
