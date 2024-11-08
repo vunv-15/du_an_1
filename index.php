b67b1dc30c59879b7b8c9af66258c0a99694434f
vunv-15 <nguyenvu15309@gmail.com>
2024-11-08 08:58:00 +0700

    init project

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+require_once './controllers/HomeController.php';
+
+// Require toàn bộ file Models
+require_once './models/Student.php';
+
+// Route
+$act = $_GET['act'] ?? '/';
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+
+match ($act) {
+    // Trang chủ
+    
+};
